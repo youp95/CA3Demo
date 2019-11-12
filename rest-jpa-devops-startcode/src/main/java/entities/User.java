0ae0fc092f5d113812b2d1244b06278b8f413632
shpat <bareshpati@hotmail.com>
2019-11-12 10:01:35 +0100

    first commit

---
+++ rest-jpa-devops-startcode/src/main/java/entities/User.java
@@ -0,0 +1,106 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.mindrot.jbcrypt.BCrypt;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  @Id
+  @Basic(optional = false)
+  @NotNull
+  @Column(name = "user_name", length = 25)
+  private String userName;
+  @Basic(optional = false)
+  @NotNull
+  @Size(min = 1, max = 255)
+  @Column(name = "user_pass")
+  private String userPass;
+  @JoinTable(name = "user_roles", joinColumns = {
+    @JoinColumn(name = "user_name", referencedColumnName = "user_name")}, inverseJoinColumns = {
+    @JoinColumn(name = "role_name", referencedColumnName = "role_name")})
+  @ManyToMany
+  private List<Role> roleList = new ArrayList();
+
+
+// gensalt's log_rounds parameter determines the complexity
+// the work factor is 2**log_rounds, and the default is 10
+  
+
+  public List<String> getRolesAsStrings() {
+    if (roleList.isEmpty()) {
+      return null;
+    }
+    List<String> rolesAsStrings = new ArrayList();
+    for (Role role : roleList) {
+      rolesAsStrings.add(role.getRoleName());
+    }
+    return rolesAsStrings;
+  }
+
+  public User() {}
+
+  //TODO Change when password is hashed
+  // Hash a password for the first time
+
+
+// Check that an unencrypted password matches one that has
+// previously been hashed
+
+        
+   public boolean verifyPassword(String pw){
+        return (BCrypt.checkpw(pw,userPass));
+       
+    }
+
+  public User(String userName, String userPass) {
+    this.userName = userName;
+    String hashed1 = BCrypt.hashpw(userPass, BCrypt.gensalt(12));
+    this.userPass = hashed1;
+   
+  }
+
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getUserPass() {
+    return this.userPass;
+  }
+
+  public void setUserPass(String userPass) {
+    this.userPass = userPass;
+  }
+
+  public List<Role> getRoleList() {
+    return roleList;
+  }
+
+  public void setRoleList(List<Role> roleList) {
+    this.roleList = roleList;
+  }
+
+  public void addRole(Role userRole) {
+    roleList.add(userRole);
+  }
+
+}
